78fe2cdb07e141b8843a2df258cfe2489922f9f8
PiotrB95 <piotr.bubnowski95@gmail.com>
2025-01-06 14:49:48 +0100

    Renamed main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 export const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <p>Main Page</p>
+      <p>Car Shop</p>
     </QueryClientProvider>
   )
 }
