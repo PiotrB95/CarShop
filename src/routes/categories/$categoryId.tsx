85a84c665e9ac3a84c7769bb4c9101652fa1d6b6
PiotrB95 <piotr.bubnowski95@gmail.com>
2025-01-10 17:18:27 +0100

    Routing tests v5

--- src/routes/categories/$categoryId.tsx
+++ src/routes/categories/$categoryId.tsx
@@ -2,7 +2,16 @@ import { createFileRoute } from '@tanstack/react-router'
 
 const CategoryView = () => {
   const params = Route.useParams()
-  return <h3>Witaj w kategorii: {params.categoryId} </h3>
+  return (
+    <div>
+      <h3>Witaj w kategorii: {params.categoryId} </h3>
+      <h4>Lista części:</h4>
+      <ul>
+        <li>1.</li>
+        <li>2.</li>
+      </ul>
+    </div>
+  )
 }
 
 export const Route = createFileRoute('/categories/$categoryId')({
